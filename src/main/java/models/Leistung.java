e1e4e76b800477847d6ded2a7abec8e5d51536c7
mvd <mvd@acadon.de>
2026-06-01 11:15:56 +0200

    created Leistung model class

--- src/main/java/models/Leistung.java
+++ src/main/java/models/Leistung.java
@@ -1,4 +1,39 @@
 package models;
 
 public class Leistung {
+
+    private String lkNr;
+    private String bezeichnung;
+    private String beschreibung;
+
+    Leistung(String lkNr, String bezeichnung, String beschreibung) {
+        this.lkNr = lkNr;
+        this.bezeichnung = bezeichnung;
+        this.beschreibung = beschreibung;
+    }
+
+    public String getLkNr() {
+        return lkNr;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setLkNr(String lkNr) {
+        this.lkNr = lkNr;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+    
 }
