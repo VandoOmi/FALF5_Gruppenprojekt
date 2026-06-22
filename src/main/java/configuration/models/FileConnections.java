513a1885823b35e4256e65ae87c242cb799e7ae3
Achilles6545 <spamspam987684@gmail.com>
2026-06-22 10:37:26 +0200

    fix spelling

--- src/main/java/configuration/models/FileConnections.java
+++ src/main/java/configuration/models/FileConnections.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FileConnections {
-    private ArrayList<FileConnection> fielConnectionList;
+    private ArrayList<FileConnection> fileConnectionList;
 
     public ArrayList<FileConnection> getFileConnectionList() {
-        return fielConnectionList;
+        return fileConnectionList;
     }
 
     public Map createFileConnectionMap() {
         HashMap connectionMap = new HashMap<ModelType, FileConnection>();
-        for (FileConnection connection : fielConnectionList) {
+        for (FileConnection connection : fileConnectionList) {
             connectionMap.put(connection.getModel(), connection);
         }
         return connectionMap;
