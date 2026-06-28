4e04ebebb5f369a80176ea745d342aef85239983
Achilles6545 <spamspam987684@gmail.com>
2026-06-28 19:07:06 +0200

    create sceleton methods

--- src/main/java/dataLayer/services/DataLayerManager.java
+++ src/main/java/dataLayer/services/DataLayerManager.java
@@ -1,4 +1,20 @@
 package dataLayer.services;
 
 public class DataLayerManager {
+    private static DataLayerManager instance;
+    private IDataLayer dataLayer;
+
+    private DataLayerManager() {
+    }
+
+    public static DataLayerManager getInstance() {
+        if (instance == null) {
+            instance = new DataLayerManager();
+        }
+        return instance;
+    }
+
+    public IDataLayer getDataLayer() {
+        return dataLayer;
+    }
 }
