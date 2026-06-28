4460ba972a639ff24a7034451395350e079f9331
Achilles6545 <spamspam987684@gmail.com>
2026-06-28 21:53:04 +0200

    resolve datalayer service

--- src/main/java/dataLayer/services/DataLayerManager.java
+++ src/main/java/dataLayer/services/DataLayerManager.java
@@ -1,13 +1,24 @@
 package dataLayer.services;
 
+import configuration.exception.ConfigurationException;
+import configuration.models.Configuration;
+import configuration.service.*;
+
 public class DataLayerManager {
+
     private static DataLayerManager instance;
     private IDataLayer dataLayer;
 
     private DataLayerManager() {
+        try {
+            Configuration config = ConnfigurationPersistenceService.getInstance().getConfiguration();
+            this.dataLayer = DataLayerFactory.createDataLayer(config);
+        } catch (ConfigurationException e) {
+            throw new RuntimeException("DataLayer konnte nicht initialisiert werden", e);
+        }
     }
 
-    public static DataLayerManager getInstance() {
+    public static synchronized DataLayerManager getInstance() {
         if (instance == null) {
             instance = new DataLayerManager();
         }
